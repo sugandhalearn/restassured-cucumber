5d9d8c1f3a90aa15737f375570d98933d15dc8ee
sugandhalearn <sug2012btech@gmail.com>
2021-02-07 22:04:10 +0100

    artifacts

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,12 +1,14 @@
 package runners;
 
 
-        import cucumber.api.CucumberOptions;
-        import cucumber.api.junit.Cucumber;
-        import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/java/features" }, glue = { "steps" })
+@CucumberOptions(features = { "src/test/java/features" }, glue = { "steps" },
+        plugin = { "de.monochromata.cucumber.report.PrettyReports:target/cucumber" })
 public class TestRunner {
 
 }
